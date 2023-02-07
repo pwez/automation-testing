b621df077196b344698c46fd0a6e8e35f7ed064d
eytantal <developer.eytan.tal@gmail.com>
2023-02-06 18:16:57 -0600

    Added some comment documentation on page.ts

--- framework/page.ts
+++ framework/page.ts
@@ -1,8 +1,33 @@
 export interface Page {
+    /**
+     *
+     * @param url
+     */
     goto(url?: string): void;
     takeScreenshot(): void;
+
+    /**
+     * Page method for clicking buttons
+     * @param inputId
+     */
     click(inputId: string): Promise<void>;
+
+    /**
+     * Page method for toggling radio buttons
+     * @param inputId
+     */
     toggle(inputId: string): Promise<void>;
+
+    /**
+     * Page method for checking checkboxes
+     * @param inputId
+     */
     check(inputId: string): Promise<void>;
+
+    /**
+     * Page method for typing into text fields
+     * @param inputId
+     * @param inputValue
+     */
     type(inputId: string, inputValue: string): Promise<void>;
 }
